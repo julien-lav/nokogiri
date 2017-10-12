47d0d7ab90119cde710a60d0d8c95e23700eaca9
julien-lav <julien.laville@hotmail.fr>
2017-10-12 16:32:14 +0200

    mod  commit

--- exercice_1.rb
+++ exercice_1.rb
@@ -6,7 +6,7 @@ require 'rest-client'
 
 
 
-
+ville = []
 
 def get_all_the_urls_of_val_doise_townhalls()
 	doc = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
@@ -15,17 +15,18 @@ def get_all_the_urls_of_val_doise_townhalls()
 	doc.xpath('//a[@class="lientxt"]').each do |node|
 		 cityTab<<node.text
 	end
-	    get_the_email_of_a_townhal_from_its_webpage(cityTab)
+		puts  cityTab.length
+	     get_the_email_of_a_townhal_from_its_webpage(cityTab)
 end
 
   def get_the_email_of_a_townhal_from_its_webpage(villes)
  	
-	# villes.each do |ville|
-		# puts ville
+	 villes.each do |ville|
+		 puts ville
   		 page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"+ville+".html"))
 		# page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
 		puts page.css('p:contains("@")').text
-	# end 
+	 end 
 
   end 
 
--- exercice_2.rb
+++ exercice_2.rb
@@ -5,13 +5,12 @@ require 'open-uri'
 require 'rest-client'
 
 
-
 # grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
 
-nom = []
-cours = []
 
-doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")
-doc.xpath('//a[@class="price"]').each do |node|
-		 puts node.text
+doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+
+doc.xpath('//a[@class="price"]').each do |prix|
+		 puts prix.text
 	end
